960826ea7266cd9edd3997fd76cf2b7f4724a595
방성훈 (Bang Seonghun) <slimejam01@gmail.com>
2023-09-26 16:56:20 +0900

    Update App.js

--- App.js
+++ App.js
@@ -2,19 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, ma falles!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Hello, ma falles!</Text>
+
+            <StatusBar style="auto" barStyle="dark-content" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
